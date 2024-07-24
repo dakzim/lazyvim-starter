8c78c0825719bcd4ee32970e0f076f40b2744ebe
kenny <kennytbss@gmail.com>
2024-07-25 01:09:37 +0800

    im

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,15 +3,18 @@
 -- Add any additional keymaps here
 
 -- 1<C-_> 2<C-/> 体现出来的特征是 2 映射到 1，如果 1 绑定功能，1、2 都有效，无论 2 是否禁用或绑定其他功能
---
-vim.keymap.del("t", "<C-_>")
-vim.keymap.del("t", "<C-/>")
 -- vim.keymap.del("n", "<C-_>")
 -- vim.keymap.del("n", "<C-/>")
 -- vim.keymap.set("n", "<C-/>", "<cmd>echo 321<cr><esc>")
 -- vim.keymap.set("n", "<C-_>", "<cmd>echo 123<cr><esc>")
 
--- terminal 模式下使用 Alt+/ 关闭窗口
+-- terminal 模式下与 bash 冲突的快捷键处理
+-- Ctrl+/ 和 Ctrl+_ 清除，替换 Alt+/ 来关闭窗口
+vim.keymap.del("t", "<C-_>")
+vim.keymap.del("t", "<C-/>")
 vim.keymap.set("t", "<A-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+-- Ctrl+h 取消
+vim.keymap.del("t", "<C-h>")
+
 -- 行号显示开关
 vim.keymap.set("n", "<A-'>", "<cmd>set number! relativenumber!<cr>", { desc = "Toggle line numbers" })
