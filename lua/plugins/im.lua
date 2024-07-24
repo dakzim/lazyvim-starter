06a5034a662f3a90941715072bf6253a0768d81c
kenny <kennytbss@gmail.com>
2024-07-24 22:14:30 +0800

    im

---
+++ lua/plugins/im.lua
@@ -0,0 +1,16 @@
+return {
+  -- lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.sections.lualine_z = {
+        function()
+          return " " .. os.date("%H:%M:%S")
+        end,
+      }
+      opts.options.section_separators = ""
+      opts.options.component_separators = "|"
+    end,
+  },
+}
