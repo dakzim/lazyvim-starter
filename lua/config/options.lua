52cca31c1cb67a8d395611362171d978dd794d53
unknown <kennytbss@gmail.com>
2024-07-25 11:29:51 +0800

    im

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,7 @@
 if vim.g.neovide then
   -- 字体
   vim.o.guifont = "JetBrainsMonoNL Nerd Font:h12"
-  -- shift + ins 粘贴 (复制的文本以空行结尾是有问题)
+  -- shift + ins 粘贴 (复制的文本以空行结尾时有问题)
   vim.keymap.set("!", "<S-Insert>", "<C-R>+", { desc = "terminal <Shift + Ins> paste" })
   vim.keymap.set("n", "<S-Insert>", '"+p', { desc = "terminal <Shift + Ins> paste" })
 end
